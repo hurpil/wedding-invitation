1a4d190e0aa2736972813375acbfebef899850e6
hurpil <hurpil1512@gmail.com>
2026-03-23 20:51:57 +0900

    thumbnail

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -37,7 +37,7 @@ export const ShareButton = () => {
                 "//" +
                 window.location.host +
                 baseUrl +
-                "/preview_image.png",
+                "/DSC_1372_1.jpg",
               link: {
                 mobileWebUrl:
                   window.location.protocol +
