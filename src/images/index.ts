884a63ce029677d7ec0bb9cf31532d01856ecd9b
hurpil <hurpil1512@gmail.com>
2026-04-04 22:51:19 +0900

    add france0 image

--- src/images/index.ts
+++ src/images/index.ts
@@ -3,10 +3,11 @@ import coverImage from "./resize_DSC_1372_1.jpg"
 import image1 from "./resize_DSC_1361_1.jpg"
 import image2 from "./resize_DSC_1290.jpg"
 import image3 from "./resize_DSC_1323.jpg"
-import image4 from "./resize_france1.jpg"
-import image5 from "./resize_france2.jpg"
-import image6 from "./resize_france3.jpg"
-import image7 from "./resize_france4.jpg"
+import image4 from "./resize_france0.jpg"
+import image5 from "./resize_france1.jpg"
+import image6 from "./resize_france2.jpg"
+import image7 from "./resize_france3.jpg"
+import image8 from "./resize_france4.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -18,4 +19,5 @@ export const GALLERY_IMAGES = [
   image5,
   image6,
   image7,
+  image8,
 ]
