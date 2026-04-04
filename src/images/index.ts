62c638a656544eb580b4258b130324e385c3ddf0
hurpil <hurpil1512@gmail.com>
2026-04-04 22:16:33 +0900

    france image

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,8 +1,12 @@
 //import coverImage from "./cover.png"
-import coverImage from "./DSC_1372_1.jpg"
-import image1 from "./DSC_1361_1.jpg"
-import image2 from "./DSC_1290.jpg"
-import image3 from "./DSC_1323.jpg"
+import coverImage from "./resize_DSC_1372_1.jpg"
+import image1 from "./resize_DSC_1361_1.jpg"
+import image2 from "./resize_DSC_1290.jpg"
+import image3 from "./resize_DSC_1323.jpg"
+import image4 from "./resize_france1.jpg"
+import image5 from "./resize_france2.jpg"
+import image6 from "./resize_france3.jpg"
+import image7 from "./resize_france4.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -10,4 +14,8 @@ export const GALLERY_IMAGES = [
   image1,
   image2,
   image3,
+  image4,
+  image5,
+  image6,
+  image7,
 ]
