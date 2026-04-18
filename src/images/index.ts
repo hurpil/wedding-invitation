1a7f597ea72cfa55076b7059ac9e197bb377cef9
hurpil <hurpil1512@gmail.com>
2026-04-19 02:04:21 +0900

    update photo 2

--- src/images/index.ts
+++ src/images/index.ts
@@ -6,14 +6,15 @@ import image2 from "./resize_wedding-20.jpg"
 import image3 from "./resize_wedding-10-1.jpg"
 import image4 from "./resize_wedding-8-1.jpg"
 import image5 from "./resize_wedding-1.jpg"
-import image6 from "./resize_DSC_1361_1.jpg"
-import image7 from "./resize_DSC_1290.jpg"
-import image8 from "./resize_DSC_1323.jpg"
-import image9 from "./resize_france0.jpg"
-import image10 from "./resize_france1.jpg"
-import image11 from "./resize_france2.jpg"
-import image12 from "./resize_france3.jpg"
-import image13 from "./resize_france4.jpg"
+import image6 from "./resize_DSC_1372_1.jpg"
+import image7 from "./resize_DSC_1361_1.jpg"
+import image8 from "./resize_DSC_1290.jpg"
+import image9 from "./resize_DSC_1323.jpg"
+import image10 from "./resize_france0.jpg"
+import image11 from "./resize_france1.jpg"
+import image12 from "./resize_france2.jpg"
+import image13 from "./resize_france3.jpg"
+import image14 from "./resize_france4.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -31,4 +32,5 @@ export const GALLERY_IMAGES = [
   image11,
   image12,
   image13,
+  image14,
 ]
